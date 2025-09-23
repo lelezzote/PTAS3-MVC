ab95691b68b296645894529acce6961e6e1a91d3
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2025-09-23 17:40:11 -0400

    varias alteracoes

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+const usuarioRoutes = require("./routes/usuarioRoutes");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
-
-app.use(cors());
 app.use(express.json());
 
-// importa as rotas de autenticação
-const authRoutes = require("./routes/authRoutes");
+// Rotas
+app.use("/usuario", usuarioRoutes);
 
-// usa as rotas de autenticação com prefixo /auth
-app.use("/auth", authRoutes);
-
-// (essa rota /enviar pode continuar se você quiser)
-app.post("/enviar", (req, res) => {
-  const { nome, email } = req.body;
-  console.log("Dados recebidos:", nome, email);
-
-  res.json({ message: `Obrigado, ${nome}! Seu email (${email}) foi recebido.` });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- routes/admRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const UsuarioController = require("../controllers/UsuarioController");
-
-router.post("/cadastro", UsuarioController.cadastrar);
-
-router.post("/auth/login", UsuarioController.login);
-
-module.exports = router;
